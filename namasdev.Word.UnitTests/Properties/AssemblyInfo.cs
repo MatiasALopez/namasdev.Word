34d9978fd934bb1be570ab0b84cfcf2af6c7aae2
Matias Lopez <matutelopez@hotmail.com>
2024-08-23 16:18:08 -0300

    [main] Commit inicial

---
+++ namasdev.Word.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("namasdev.Word.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("namasdev.Word.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30b87ccf-7f59-4a1f-ba61-3f0598c4d8ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ namasdev.Word/WordHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Text.RegularExpressions;
+
+using DocumentFormat.OpenXml.Packaging;
+
+using namasdev.Core.IO;
+
+namespace namasdev.Word
+{
+    public class WordHelper
+    {
+        private const string WORD_NEW_LINE = "<w:br/>";
+
+        public static Archivo GenerarWordDesdeTemplateReemplazandoCampos(
+            Archivo template, Dictionary<string,string> campos)
+        {
+            using (var stream = new MemoryStream())
+            {
+                // NOTA (ML): necesitamos inicializar/cargar el MemoryStream de esta manera para que sea resizable
+                stream.Write(template.Contenido, 0, template.Contenido.Length);
+
+                using (var doc = WordprocessingDocument.Open(stream, isEditable: true, new OpenSettings { AutoSave = false }))
+                {
+                    string docText = null;
+                    using (var sr = new StreamReader(doc.MainDocumentPart.GetStream()))
+                    {
+                        docText = sr.ReadToEnd();
+                    }
+
+                    docText = Regex.Replace(
+                        docText, 
+                        @"{{(\w+)}}", 
+                        (m) => 
+                        {
+                            string valor;
+                            return campos.TryGetValue(m.Groups[1].Value, out valor)
+                                ? FormatearValor(valor)
+                                : String.Empty;
+                        });
+
+                    using (var wr = new StreamWriter(doc.MainDocumentPart.GetStream(FileMode.Create)))
+                    {
+                        wr.Write(docText);
+                    }
+                    
+                    template.Contenido = stream.ToArray();
+
+                    return template;
+                }
+            }
+        }
+
+        private static string FormatearValor(string valor)
+        {
+            return SecurityElement.Escape(valor)
+                .Replace(Environment.NewLine, WORD_NEW_LINE);
+        }
+    }
+}
